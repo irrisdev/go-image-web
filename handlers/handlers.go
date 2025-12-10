f7b3a41765084e35de65bed591c4a32f9a79987e
Irris <160921248+irrisdev@users.noreply.github.com>
2025-12-10 10:14:06 +0000

    feat: add image upload form to index page

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -12,7 +12,9 @@ var baseLayout = template.Must(template.ParseFiles(path.Join(publicDir, "layout.
 func IndexPage(w http.ResponseWriter, r *http.Request) {
 	tpl := template.Must(template.Must(baseLayout.Clone()).
 		ParseFiles(path.Join(publicDir, "index.html")))
+
 	data := services.IndexService()
+
 	if err := tpl.ExecuteTemplate(w, "layout", data); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
@@ -23,5 +25,18 @@ func ServeStaticAsset(w http.ResponseWriter, r *http.Request) {
 }
 
 func UploadAsset(w http.ResponseWriter, r *http.Request) {
+	file, header, err := r.FormFile("imageFile")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+	_ = header
+	// sanitise filename
+
+	// save file
+
+	// redirect to index pager / new upload
+	http.Redirect(w, r, "/", http.StatusMovedPermanently)
 
 }

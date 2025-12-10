41c7ab7b95988a09ef9da221eea9750cf5963f84
Oliver Dennis <oliver.d.333@proton.me>
2025-12-10 10:24:06 +0000

    Fix

--- store/store.go
+++ store/store.go
@@ -4,4 +4,4 @@ import "go-image-web/models"
 
 var IndexPageDataStore = models.IndexPageModel{}
 var AssetsFolder string = "public/assets"
-var ImageAssetsFolder string = "public/assets"
+var ImageAssetsFolder string = "public/assets/images"
